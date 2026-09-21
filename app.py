d33310556c0c38828cbd301e0842c50f67b22ef8
senai_25_26 <geovana.tomaz@aluno.senai.br>
2026-09-21 09:22:27 -0300

    WEB

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from flask import Flask, render_template, request, flash, redirect, url_for
+from flask_login import LoginManager, UserMixin
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
@@ -20,6 +21,10 @@ def adocao():
 def denuncia():
     return render_template("denuncia.html")
 
+@app.route('/denuncia_anonima')
+def denuncia_anonima():
+    return render_template("denuncia_anonima.html")
+
 @app.route('/ongs')
 def ongs():
     return render_template("ongs.html")
@@ -30,8 +35,24 @@ def cadastro():
 
 @app.route('/cadastro_ong')
 def cadastro_ong():
+    print("Cadastro de ong")
     return render_template("cadastro_ong.html")
 
+@app.route('/cadastro_animal')
+def cadastro_animal():
+    return render_template("cadastro_animal.html")
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+@app.route('/login_ong')
+def login_ong():
+    return render_template("login_ong.html")
+
+@app.route('/ver_denuncias')
+def ver_denuncias():
+    return render_template("ver_denuncias.html")
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5003, host='0.0.0.0')
+    app.run(debug=True, port=5030, host='0.0.0.0')
